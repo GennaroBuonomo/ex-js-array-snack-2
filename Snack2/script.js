cff10e8774ecdb65f6e75094f5f1cd4b9f95239b
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-11-11 13:58:45 +0100

    Add initial HTML and JavaScript files for Snack2 project

---
+++ Snack2/script.js
@@ -0,0 +1,79 @@
+const books = [
+  { 
+	  title: "React Billionaire", 
+	  pages: 250, 
+	  author: {
+		  name: 'Alice',
+		  age: 35
+	  },
+	  available: false,
+	  price: '101€',
+	  tags: ['advanced', 'js', 'react', 'senior']
+  },
+  { 
+	  title: "Advanced JS", 
+	  pages: 500, 
+	  author: {
+		  name: 'Bob',
+		  age: 20
+	  },
+	  available: true,
+	  price: '25€',
+	  tags: ['advanced', 'js', 'mid-senior']
+  },
+  { 
+	  title: "CSS Secrets", 
+	  pages: 320, 
+	  author: {
+		  name: 'Alice',
+		  age: 17
+	  },
+	  available: true,
+	  price: '8€',
+	  tags: ['html', 'css', 'junior']
+  },
+  { 
+	  title: "HTML Mastery", 
+	  pages: 200, 
+	  author: {
+		  name: 'Charlie',
+		  age: 50
+	  },
+	  available: false,
+	  price: '48€',
+	  tags: ['html', 'advanced', 'junior', 'mid-senior']
+  },
+];
+
+//Creare un array (availableBooks) che contiene tutti i libri disponibili.
+//Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+//Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
+
+const availableBooks = books.filter(books => books.available);
+console.log(availableBooks);
+
+const discountedBooks = availableBooks.map(book => {
+  const priceString = book.price;
+  const priceValue = parseFloat(priceString); 
+  const currency = priceString.replace(priceValue, '');
+
+  const discountedValue = priceValue * 0.8; 
+  const roundedValue = discountedValue.toFixed(2); 
+
+  const newPriceString = roundedValue + currency;
+
+  return {
+    ...book, 
+    price: newPriceString 
+  };
+});
+
+console.log(discountedBooks);
+
+const fullPricedBook = discountedBooks.find(book => {
+  const priceValue = parseFloat(book.price);
+  return Number.isInteger(priceValue);
+});
+
+console.log('Primo libro con prezzo intero scontato');
+console.log(fullPricedBook);
