355539278f41013c46894f8132d736c30a139ae3
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-11-26 17:21:48 +0100

    Fix sorting logic for authors' ages and improve comments for clarity in script.js

--- Snack3/script.js
+++ Snack3/script.js
@@ -49,10 +49,14 @@ const books = [
 //Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
 //Ordina l’array authors in base all’età, senza creare un nuovo array.                              (se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
 
+
+//Ho messo map perche mi chiedeva di prendere gli a utori dei libri
 const authors = books.map(books => books.author);
 console.log(authors)
 
+//Ho inserito .EVERY che mi controlla tutti gli elementi dell array per vedere se soddisfano le condizioni ma basta solo uno che non soddisfa la condizione e mi restituisce false ma se la soddisfa restituisce true poi ho usato l operatore ternario (e come un if/else scritto in una sola riga SI(?) usa il numero 1 NO(:) usa il numero -1) Poi ce il .SORT che mi serve per ordinare l eta in modo crescente
+
 const areAuthorsAdults = authors.every(book => book.age >= 18);
-authors.sort((a, b) => a.age - b.age * (areAuthorsAdults ? 1 : -1)) 
+authors.sort((a, b) => (a.age - b.age) * (areAuthorsAdults ? 1 : -1)) 
 
 console.log(authors)
